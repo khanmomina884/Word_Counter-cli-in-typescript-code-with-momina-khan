e2e5efadceda469664d1f1a99f4fb81528937c33
Momina Khan <160432561+khanmomina884@users.noreply.github.com>
2024-05-23 14:02:22 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+//importing inquirer and chalk packeges
+import inquirer from "inquirer";
+import chalk from "chalk";
+//display acolorful welcome message 
+console.log(chalk.bold.bgMagenta(' \n \t \t Code With Momina - Word Counter \n'));
+console.log("=".repeat(60));
+//prompt the user to enter a sentence 
+let answers = await inquirer.prompt([{
+        name: 'sentence',
+        type: "input",
+        message: "Enter a Sentence",
+    }]);
+//trimig the sentence and splittting it into words based on"spaces",
+let words = answers.sentence.trim().split(" ");
+//analysis of user input sentence
+console.log("=".repeat(60));
+console.log(chalk.bold(`- Sentence Words:`));
+console.log(words);
+console.log(chalk.bold(`\n - Word Count: ${chalk.bold.bgRedBright(words.length)}`));
+console.log("=".repeat(60));
